fc3630f58e38bf44227af1c7174701691c68b79c
natanimabesha <natanimabesha@www.zhewutech.com>
2022-10-17 20:12:27 +0300

    Setup webpacks

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/js/index.js',
+    },
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    output: {
+        filename: 'index.bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/Javascript-capstone-project/dist',
+        clean: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+
+        ],
+
+    },
+    mode: 'development',
+};
