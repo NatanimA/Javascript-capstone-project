e6a551fe257e940bfe84c35f241b3b2fbe9a17f0
natanimabesha <natanimabesha@www.zhewutech.com>
2022-10-18 11:48:40 +0300

    Fix linters

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import '../style.css';
-import imgLogo from '../assets/century-logo.png'
+import imgLogo from '../assets/century-logo.png';
 
-const logo = document.querySelector('#logo-container')
+const logo = document.querySelector('#logo-container');
 
-logo.innerHTML = `<a href="#"><img class="logo-image" src="${imgLogo}" alt="century-cinema"></a>`
+logo.innerHTML = `<a href="#"><img class="logo-image" src="${imgLogo}" alt="century-cinema"></a>`;
