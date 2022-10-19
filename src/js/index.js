71f24c28468d1013e0ef9808b345cb5c510ff090
natanimabesha <natanimabesha@www.zhewutech.com>
2022-10-19 11:02:40 +0300

    Fix Comments skeleton

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,12 @@ import '../style.css';
 import imgLogo from '../assets/century-logo.png';
 import DisplayMovies from './modules/apiUtility.js';
 import FetchandInsertMovies from './modules/display.js';
+import PopUpComment from './modules/comment.js';
 
 const logo = document.querySelector('#logo-container');
+const commentSection = document.querySelector('.comments-section')
+const homepage = document.querySelector('.homepage')
+const commentsDetail = document.querySelector('.comments-detail')
 
 logo.innerHTML = `<a href="#"><img class="logo-image" src="${imgLogo}" alt="century-cinema"></a>`;
 
@@ -15,3 +19,19 @@ class Homepage {
 }
 
 Homepage.loadMovies();
+
+homepage.addEventListener('click',async(event) => {
+  if (event.target.className === 'comment-btn'){
+    const commentId = event.target.id;
+    commentSection.style.display='block'
+    homepage.classList.add('active')
+    await PopUpComment.commentPopUp(commentId);
+  }
+})
+
+
+commentSection.addEventListener('click', async (event) => {
+  if(event.target.className === "close-btn"){
+    commentSection.style.display='none'
+  }
+})
