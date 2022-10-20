c243c3dd8fcdb8efe3bed10401b4d3ca938e06ae
natanimabesha <natanimabesha@www.zhewutech.com>
2022-10-20 15:34:30 +0300

    Fix linters

--- src/__tests__/commentsCounter.test.js
+++ src/__tests__/commentsCounter.test.js
@@ -1,25 +1,21 @@
 /**
  * @jest-environment jsdom
  */
-const involvmentApi =
-  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/3CNfK8xiNLfIWKUKw5ck';
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve([
-        {
-          comment: 'Loved the movie',
-          creation_date: '2022-05-10',
-          username: 'Ashwin',
-        },
-        {
-          comment: 'Absoulutley a banger!',
-          creation_date: '2022-02-20',
-          username: 'Natanim',
-        },
-      ]),
-  })
-);
+const involvmentApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/3CNfK8xiNLfIWKUKw5ck';
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve([
+    {
+      comment: 'Loved the movie',
+      creation_date: '2022-05-10',
+      username: 'Ashwin',
+    },
+    {
+      comment: 'Absoulutley a banger!',
+      creation_date: '2022-02-20',
+      username: 'Natanim',
+    },
+  ]),
+}));
 const commentsCounter = async (id) => {
   const response = await fetch(`${involvmentApi}/comments?item_id=${id}`);
   const numberOfComments = await response.json();
@@ -30,9 +26,9 @@ const commentsCounter = async (id) => {
 };
 describe('Tests the comment count', () => {
   test('should test the number of comments', async () => {
-    document.body.innerHTML = `<span class="comments-counter"></span>`;
+    document.body.innerHTML = '<span class="comments-counter"></span>';
     const count = document.querySelector('.comments-counter');
     await commentsCounter(1);
-    expect(count.innerHTML).toEqual(`(2)`);
+    expect(count.innerHTML).toEqual('(2)');
   });
 });
